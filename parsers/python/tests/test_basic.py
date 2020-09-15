5b1c1e90578ce8ba1d8a8c83246054e295c4ccb6
etienne <etnnth@gmail.com>
2020-09-15 15:22:34 +0200

    add two lists test

--- parsers/python/tests/test_basic.py
+++ parsers/python/tests/test_basic.py
@@ -29,3 +29,11 @@ def test_list_at_root():
 def test_list_at_first_level():
 	lines = ["root", "\titem1", "\titem2"]
 	assert pytaml.parse_lines(lines) == [("root", ["item1", "item2"])]
+
+
+def test_two_lists():
+	lines = ["root1", "\titem1", "\titem2", "root2", "\titem3", "\titem4"]
+	assert pytaml.parse_lines(lines) == [
+											("root1", ["item1", "item2"]),
+											("root2", ["item3", "item4"])
+										]
