cbb42be3634ecdfc29f17b50394dcc7340be8d7a
Kahbazi <akahbazi@gmail.com>
2020-11-22 19:14:28 +0330

    Add Configuration.Taml.Net (#74)
    * Add Configuration.Taml.Net

---
+++ src/dotnet/Configuration.Taml.NET/Configuration.Taml.NET/TamlStreamConfigurationSource.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.Configuration;
+
+namespace Configuration.Taml.NET
+{
+	/// <summary>
+	/// Represents a Taml file as an <see cref="IConfigurationSource"/>.
+	/// </summary>
+	public class TamlStreamConfigurationSource : StreamConfigurationSource
+	{
+		/// <summary>
+		/// Builds the <see cref="TamlStreamConfigurationProvider"/> for this source.
+		/// </summary>
+		/// <param name="builder">The <see cref="IConfigurationBuilder"/>.</param>
+		/// <returns>An <see cref="TamlStreamConfigurationProvider"/></returns>
+		public override IConfigurationProvider Build(IConfigurationBuilder builder)
+			=> new TamlStreamConfigurationProvider(this);
+	}
+}
