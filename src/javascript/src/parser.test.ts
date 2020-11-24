17635a49d5f0747ec78331d4c6334cef355dad7d
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2020-11-24 13:02:31 -0500

    Added first tests for TamlArray

--- src/javascript/src/parser.test.ts
+++ src/javascript/src/parser.test.ts
@@ -3,6 +3,7 @@ import * as chai from 'chai';
 
 import { Parser } from './parser';
 import { TamlDocument } from './TamlDocument';
+import { TamlArray } from './TamlArray';
 
 const expect = chai.expect;
 describe('Parser', () => {
@@ -17,5 +18,36 @@ key2	value2`;
 		var outDoc = Parser.Parse(onlyKeyValuePairs);
 		expect(outDoc.KeyValuePairs.length).is.equal(2, "Did not find 2 KeyValuePairs");
 	});
+
+	const extraTabs = `key1			value1						`;
+	it('should strip off extra tabs from the end of a value', () => {
+		var kvp = Parser.Parse(extraTabs).KeyValuePairs[0];
+		expect(kvp.Value).is.equal("value1");
+	});
+
+	const simpleArray = `key
+value1
+value2
+value3						`
+it('should detect and return a simple array', () => {
+	const kvp = Parser.Parse(simpleArray).KeyValuePairs[0];
+	expect(kvp.hasValue).is.true;
+	const arr = kvp.Value as TamlArray;
+	expect(arr).is.not.null;
+	expect(arr.length).is.equal(3);
+	expect(arr[0].toString()).is.equal("value1");
+	expect(arr[1].toString()).is.equal("value2");
+	expect(arr[2].toString()).is.equal("value3");
+});
+
+	// Spec 1.1 features
+
+// 	const commentsInDoc = `key1			value1
+// # This is a comment					comments are to be ignored
+// key2			value2`
+// 	it('should ignore lines that start with #', () => {
+// 		var outDoc = Parser.Parse(commentsInDoc); 
+// 		expect(outDoc.KeyValuePairs.length).is.equal(2, "Parsed and did something with the comment");
+// 	});
 
 });
--- src/javascript/src/parser.ts
+++ src/javascript/src/parser.ts
@@ -1,5 +1,7 @@
+import { TamlArray } from "./TamlArray";
 import { TamlDocument } from "./TamlDocument";
 import { TamlKeyValuePair } from "./TamlKeyValuePair";
+import { ITamlValue, TamlValue } from "./TamlValue";
 
 export class Parser {
 
@@ -12,14 +14,39 @@ static _SingleValue = /^\t*\S[^\t]*\t*$/gi;
 		var outDoc = new TamlDocument();
 		var lines = taml.split('\n');
 
+		var value:ITamlValue|null = null;
 		for (const line of lines) {
+
+			// This line is a comment if it starts with #
+			if (line[0] === "#") continue;
+
+			// Skip empty lines
+			if (line.trim() === "") continue;
+
 			if (TamlKeyValuePair.IsKeyValuePair(line)) {
 				var newPair = TamlKeyValuePair.fromString(line);
 				if (newPair != null) outDoc.KeyValuePairs.push(newPair);
+			} else {  // This is JUST a value
+				
+				if (value === null) {
+					const kvp = new TamlKeyValuePair();
+					kvp.Key = line.trim();
+					value = kvp;
+					outDoc.KeyValuePairs.push(kvp);
+				} else {
+					if (value instanceof TamlKeyValuePair) {
+						// console.log("Adding entry to the KVP");
+						const kvp:TamlKeyValuePair = value as TamlKeyValuePair;
+						if (!kvp.hasValue) {
+							kvp.Value = new TamlArray();
+						}
+						const arr:TamlArray = kvp.Value as TamlArray;
+						arr.push(new TamlValue(line.trim()));
+					}
+				}
+
 			}
 		}
-
-		console.log("I did a thing!  Boop!");
 
 		return outDoc;
 		
